dd001cdb088e41e1a471641069a2b216bcf49e47
stanley <stanleygomesdasilva@gmail.com>
2020-01-07 15:23:05 -0300

    organizing main module

--- src/main/asgi.py
+++ src/main/asgi.py
@@ -1,16 +1,6 @@
-"""
-ASGI config for app project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
-"""
-
+# ASGI config for app project.
 import os
-
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
-
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
 application = get_asgi_application()
--- src/main/wsgi.py
+++ src/main/wsgi.py
@@ -1,16 +1,6 @@
-"""
-WSGI config for app project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
-"""
-
+# WSGI config for app project.
 import os
-
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
-
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
 application = get_wsgi_application()
